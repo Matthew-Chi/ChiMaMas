7184bd8f90458848e95b50c1181de3098521d1b1
squishyTofu <luj3@bxscience.edu>
2024-01-16 09:23:54 -0500

    last time everything

--- flutter_application1/lib/gallery.dart
+++ flutter_application1/lib/gallery.dart
@@ -13,28 +13,30 @@ class _GalleryPageState extends State<GalleryPage> {
 
   @override
   @override
-  // void initState() {
-  //   super.initState();
-  //   // Set up a timer to simulate automatic scrolling
-  //   Timer.periodic(Duration(seconds: 2), (timer) {
-  //     if (_currentIndex < 7) {
-  //       _currentIndex++;
-  //     } else {
-  //       _currentIndex = 0;
-  //       // Smooth transition back to the first picture
-  //       _scrollController.animateTo(
-  //         0.0,
-  //         duration: Duration(seconds: 1),
-  //         curve: Curves.easeInOut,
-  //       );
-  //     }
-  //     _scrollController.animateTo(
-  //       _currentIndex * 205.0, // Adjusted based on the width of each item
-  //       duration: Duration(seconds: 1),
-  //       curve: Curves.easeInOut,
-  //     );
-  //   });
-  // }
+  void initState() {
+    super.initState();
+    // Set up a timer to simulate automatic scrolling
+    Future.delayed(Duration(seconds: 2), () {
+      Timer.periodic(Duration(seconds: 2), (timer) {
+        if (_currentIndex < 7) {
+          _currentIndex++;
+        } else {
+          _currentIndex = 0;
+          // Smooth transition back to the first picture
+          _scrollController.animateTo(
+            0.0,
+            duration: Duration(seconds: 1),
+            curve: Curves.easeInOut,
+          );
+        }
+        _scrollController.animateTo(
+          _currentIndex * 205.0, // Adjusted based on the width of each item
+          duration: Duration(seconds: 1),
+          curve: Curves.easeInOut,
+        );
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -140,50 +142,24 @@ class _GalleryPageState extends State<GalleryPage> {
             padding: const EdgeInsets.only(top: 15),
             child: Container(
               height: 145,
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  WidgetsBinding.instance?.addPostFrameCallback((_) {
-                    Timer.periodic(Duration(seconds: 2), (timer) {
-                      if (_currentIndex < 7) {
-                        _currentIndex++;
-                      } else {
-                        _currentIndex = 0;
-                        // Smooth transition back to the first picture
-                        _scrollController.animateTo(
-                          0.0,
-                          duration: Duration(seconds: 1),
-                          curve: Curves.easeInOut,
-                        );
-                      }
-                      _scrollController.animateTo(
-                        _currentIndex * 205.0,
-                        duration: Duration(seconds: 1),
-                        curve: Curves.easeInOut,
-                      );
-                    });
-                  });
-
-                  return ListView.builder(
-                    controller: _scrollController,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 12,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                                width: 4,
-                                color: Color.fromARGB(255, 212, 130, 8)),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          width: 200,
-                          child: Image.asset(
-                            'images/pic${index % 11 + 1}.png',
-                          ),
-                        ),
-                      );
-                    },
+              child: ListView.builder(
+                controller: _scrollController,
+                scrollDirection: Axis.horizontal,
+                itemCount: 12,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            width: 4, color: Color.fromARGB(255, 212, 130, 8)),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      width: 200,
+                      child: Image.asset(
+                        'images/pic${index % 11 + 1}.png',
+                      ),
+                    ),
                   );
                 },
               ),
